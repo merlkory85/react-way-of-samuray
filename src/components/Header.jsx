4a133eafb07b2ab657a66b916687a3c51c86ccbd
Yavorskyi Serhii <merlkory1985@gmai.com>
2021-03-25 23:27:11 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="http://zornet.ru/_fr/83/1291083.jpg" alt="" />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://prikolnye-kartinki.ru/img/picture/Aug/05/0946cd5b3323e1287ade87d80ef1b240/1.jpg" alt="" />
+      </div>
+      <div>
+        ava + description
+    </div>
+      <div>
+        My posts
+      <div>
+          New post
+      </div>
+        <ul>
+          <li>
+            Post 1
+        </li>
+          <li>
+            Post 2
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
